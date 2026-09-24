37363cea247fcaf7486b4766b780569ee86472b1
Pavan Kumar <channapavan2006@gmail.com>
2026-09-24 23:16:12 +0530

    Connect frontend to deployed API

--- frontend/app_script.js
+++ frontend/app_script.js
@@ -75,22 +75,13 @@ form.addEventListener(
             // SEND DATA TO FASTAPI
             // ====================================
 
-            const response = await fetch(
-                "http://127.0.0.1:8000/predict",
-                {
-
-                    method: "POST",
-
-                    headers: {
-
-                        "Content-Type":
-                            "application/json"
-                    },
-
-                    body:
-                        JSON.stringify(houseData)
-                }
-            );
+            const response = await fetch("https://house-price-prediction-api-kwtj.onrender.com/predict", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                        },
+                body: JSON.stringify(houseData)
+            });
 
 
             // ====================================
